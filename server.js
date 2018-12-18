dd06e15a4a7faafb3579441e39ef02bf78516eae
JavaSampleApproach <javasampleapproach@gmail.com>
2018-12-18 08:58:42 +0000

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 8080
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
