93313a308de8b0e05b1590dd2c79b8eb3780af7b
JavaSampleApproach <javasampleapproach@gmail.com>
2018-12-18 14:34:05 +0000

    upadate commit

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
-const express = require('express')
-const app = express()
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json())
+ 
+const db = require('./app/config/db.config.js');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const Customer = db.customers;
+  
+// force: true will drop the table if it already exists
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync with { force: true }');
+  initial();
+});
+ 
+require('./app/route/customer.route.js')(app);
+ 
+// Create a Server
+var server = app.listen(process.env.PORT || 5000, function () {
+ 
+  var host = server.address().address
+  var port = server.address().port
+ 
+  console.log("App listening at http://%s:%s", host, port)
+})
 
-const port = process.env.PORT || 5000
+function initial() {
+	Customer.create({
+	   firstname: "Jack",
+	   lastname: "Davis",
+	   age: 25
+        });
+ 
+	Customer.create({
+	   firstname: "Mary",
+	   lastname: "Taylor",
+	   age: 37
+        });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+	Customer.create({
+	   firstname: "Mary",
+	   lastname: "Taylor",
+	   age: 37
+        });
+}
